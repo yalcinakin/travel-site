d360873fc3a19bd6887c812d5af7151d45fb78c8
Akin <yalcinakin@sabanciuniv.edu>
2018-11-30 14:43:55 +0900

    Completed Header

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename: "App.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
+
+/*
+
+*/
